2275414bf138af33cd73998048e7b0da1dfcc269
fredyakumo <fredyakumo@gmail.com>
2024-09-10 16:58:17 +0800

    Add use of spdlog

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,5 +1,16 @@
+#include "spdlog/common.h"
+#include <cstdint>
 #include <iostream>
+#include <spdlog/spdlog.h>
+
+using spdlog::info;
+using spdlog::debug;
+using spdlog::error;
 
 int main() {
-    std::cout << "server" << std::endl;
+    spdlog::set_level(spdlog::level::debug);
+
+    info("Server");
+    std::uint64_t v = 0;
+    debug("Debug variable: {0}", v);
 }
