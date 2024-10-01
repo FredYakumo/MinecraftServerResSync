9d7740002fcf51de61da3a061ab0601785b6a9b6
fredyakumo <fredyakumo@gmail.com>
2024-10-01 08:50:39 +0800

    Add util: SplitString

--- server/src/components.cpp
+++ server/src/components.cpp
@@ -1,35 +1,41 @@
 #include "components.h"
 #include "http_service.h"
 #include "main.h"
+#include <boost/url.hpp>
 
 namespace components {
-    using request =
-        boost::beast::http::request<boost::beast::http::string_body>;
-    using response =
-        boost::beast::http::response<boost::beast::http::string_body>;
+    using request = boost::beast::http::request<boost::beast::http::string_body>;
+    using response = boost::beast::http::response<boost::beast::http::string_body>;
     using verb = boost::beast::http::verb;
 
-    void get_resource_list(const request &req, response &response) {
+    void get_resource_list(const request &req, response &response, std::unordered_map<std::string_view, std::string_view> param_map) {
         boost::json::array ret{};
-        for (auto &it : g_server_data.get_const()
-                            ->class_file_resource()
-                            ->class_file_hash_map()) {
+        for (auto &it : g_server_data.get_const()->class_file_resource()->class_file_hash_map()) {
             boost::json::array file_list{};
 
             for (auto &file_it : it.second.file_hash_map()) {
-                file_list.push_back(
-                    boost::json::value{file_it.first, file_it.second});
+                file_list.push_back(boost::json::value{file_it.first, file_it.second});
             }
 
-            ret.push_back(boost::json::value{{"class_name", it.first},
-                                             {"file_list", file_list}});
+            ret.push_back(boost::json::value{{"class_name", it.first}, {"file_list", file_list}});
         }
 
         http_service::write_json_result(ret, response);
     }
 
+    void fetch_resource(const request &req, response &response, std::unordered_map<std::string_view, std::string_view> param_map) {
+        boost::json::array ret{};
+        for (auto &p_it : param_map) {
+            boost::json::object val {};
+            val["key"] = p_it.first;
+            val["value"] = p_it.second;
+            ret.push_back(val);
+        }
+        http_service::write_json_result(ret, response);
+    }
+
     void register_component_apis(http_service::HttpServer &server) {
-        server.register_api("/getResourceList",
-                            models::Api{verb::get, get_resource_list});
+        server.register_api("/getResourceList", models::Api{verb::get, get_resource_list});
+        server.register_api("/fetchResource", models::Api{verb::get, fetch_resource});
     }
 } // namespace components
--- server/src/http_service.cpp
+++ server/src/http_service.cpp
@@ -1,8 +1,14 @@
 #include "http_service.h"
 #include "components.h"
-#include <boost/beast/websocket.hpp>
 #include <boost/asio.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/url/parse.hpp>
+#include <exception>
+#include <spdlog/spdlog.h>
 #include <utility>
+// #define BOOST_URL_NO_LIB
+#include <boost/url.hpp>
+#include <utils.hpp>
 
 namespace http_service {
     namespace beast = boost::beast;   // from <boost/beast.hpp>
@@ -11,35 +17,50 @@ namespace http_service {
 
     void Session::do_read() {
         auto self = shared_from_this();
-        async_read(m_socket, m_buffer, m_req,
-                   [self](beast::error_code ec, std::size_t) {
-                       if (!ec) {
-                           self->handle_request();
-                       }
-                   });
+        async_read(m_socket, m_buffer, m_req, [self](beast::error_code ec, std::size_t) {
+            if (!ec) {
+                self->handle_request();
+            }
+        });
     }
 
     void Session::handle_request() {
-        const auto path = std::string{m_req.target()};
-        spdlog::info("Get request from: {0}, path: {1}",
-                     m_socket.remote_endpoint().address().to_string(), path);
+        const std::string_view target{m_req.target()};
+        const boost::system::result<boost::url_view> url{boost::urls::parse_uri_reference(target)};
+
+        spdlog::info("Get request from: {0}, path: {1}", m_socket.remote_endpoint().address().to_string(), target);
+        spdlog::debug("Query: {}", url->query());
 
         const auto thread_id = boost::this_thread::get_id();
         spdlog::debug("Thread id: {0}", thread_id_to_string(thread_id));
+
         http::response<http::string_body> res;
         // route
-        if (const auto &api = m_bind_apis.find(path);
-            api != m_bind_apis.cend()) {
+        if (const auto &api = m_bind_apis.find(url->path()); api != m_bind_apis.cend()) {
             if (m_req.method() != api->second.method) {
                 res.result(http::status::bad_request);
                 res.set(http::field::content_type, "text/plain");
 
-                res.body() =
-                    (boost::format("Bad request method: {}") % m_req.method())
-                        .str();
+                res.body() = (boost::format("Bad request method: {}") % m_req.method()).str();
                 res.prepare_payload();
             } else {
-                api->second.api_function(m_req, res);
+                try {
+                const std::string param = url->query();
+                const auto query_it = utils::SplitString(param, '&');
+                std::unordered_map<std::string_view, std::string_view> param_map{};
+                auto q = query_it.begin();
+                for (auto begin = query_it.begin(); begin != query_it.end(); ++begin) {
+                    utils::SplitString::Iterator p_it = utils::SplitString(*begin, '=').begin();
+                    auto key = *p_it;
+                    ++p_it;
+                    auto value = *p_it;
+                    param_map.emplace(key, value);
+                }
+
+                api->second.api_function(m_req, res, std::move(param_map));
+                } catch (const std::exception &e) {
+                    spdlog::error("{}", e.what());
+                }
             }
         } else {
             res.result(http::status::not_found);
@@ -51,16 +72,13 @@ namespace http_service {
     }
 
     void Session::do_write(http::response<http::string_body> &&res) {
-        auto sp_res =
-            std::make_shared<http::response<http::string_body>>(std::move(res));
+        auto sp_res = std::make_shared<http::response<http::string_body>>(std::move(res));
         auto self = shared_from_this();
-        http::async_write(m_socket, *sp_res,
-                          [self, sp_res](beast::error_code ec, std::size_t) {
-                              if (!ec) {
-                                  self->m_socket.shutdown(
-                                      tcp::socket::shutdown_send, ec);
-                              }
-                          });
+        http::async_write(m_socket, *sp_res, [self, sp_res](beast::error_code ec, std::size_t) {
+            if (!ec) {
+                self->m_socket.shutdown(tcp::socket::shutdown_send, ec);
+            }
+        });
     }
 
     void WebSocketSession::send_number() {
@@ -74,14 +92,13 @@ namespace http_service {
         const auto mutable_buffers = m_buffer.prepare(d_str.size());
         std::memcpy(mutable_buffers.data(), d_str.data(), d_str.size());
         m_buffer.commit(d_str.size());
-        m_ws.async_write(m_buffer.data(),
-            [self](beast::error_code ec, std::size_t /*bytes_transferred*/) {
-                if (ec) {
-                    spdlog::error("Error sending number:  {}", ec.message());
-                    self->m_disconnected = true;
-                    self->m_timer.cancel();
-                }
-            });
+        m_ws.async_write(m_buffer.data(), [self](beast::error_code ec, std::size_t /*bytes_transferred*/) {
+            if (ec) {
+                spdlog::error("Error sending number:  {}", ec.message());
+                self->m_disconnected = true;
+                self->m_timer.cancel();
+            }
+        });
         m_buffer.clear();
 
         spdlog::info("Send number: {}", m_counter.get_const());
@@ -95,18 +112,17 @@ namespace http_service {
     }
 
     void HttpServer::do_accept() {
-        m_acceptor.async_accept(
-            [this](beast::error_code ec, tcp::socket socket) {
-                if (!ec) {
-                    std::make_shared<Session>(std::move(socket), m_bind_apis)
-                        ->start();
-                    // std::make_shared<WebSocketSession>(std::move(socket), m_ioc)
-                    //     ->start();
-                }
+        m_acceptor.async_accept([this](beast::error_code ec, tcp::socket socket) {
+            if (!ec) {
+                std::make_shared<Session>(std::move(socket), m_bind_apis)->start();
+                // std::make_shared<WebSocketSession>(std::move(socket),
+                // m_ioc)
+                //     ->start();
+            }
 
-                // keep listening
-                do_accept();
-            });
+            // keep listening
+            do_accept();
+        });
     }
 
     void HttpServer::start_threads() {
@@ -121,22 +137,14 @@ namespace http_service {
 
     //------------------------------------------------------------------------------
 
-    result::Result start_service(const std::string_view host_address,
-                                 uint16_t port, int threads_count) {
-        spdlog::info("Start http daemon: {0}:{1}, with {2} thread(s)",
-                     host_address, port, threads_count);
+    result::Result start_service(const std::string_view host_address, uint16_t port, int threads_count) {
+        spdlog::info("Start http daemon: {0}:{1}, with {2} thread(s)", host_address, port, threads_count);
         try {
             HttpServer server(host_address, port, threads_count);
 
-            server.register_api(
-                "/test",
-                models::Api{http::verb::get,
-                            [](const http::request<http::string_body> &,
-                               http::response<http::string_body> &res) {
-                                write_json_result(
-                                    boost::json::value{{"result", "success"}},
-                                    res);
-                            }});
+            server.register_api("/test", models::Api{http::verb::get, [](const http::request<http::string_body> &, http::response<http::string_body> &res, std::unordered_map<std::string_view, std::string_view>) {
+                                                         write_json_result(boost::json::value{{"result", "success"}}, res);
+                                                     }});
             components::register_component_apis(server);
             server.run();
         } catch (const std::exception &e) {
