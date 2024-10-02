ed657d8b354f3e64503f13ed55a64713278739ee
fredyakumo <fredyakumo@gmail.com>
2024-10-03 05:28:15 +0800

    Rearrange the project file structure

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
+#include "main.h"
 
-int main() { std::cout << "client" << std::endl; }
+
+using models::ManageClassPathMap;
+using models::ServerData;
+using models::ShareMutexData;
+
+ShareMutexData<std::shared_ptr<ManageClassPathMap>> g_manage_class_path_map{
+    nullptr};
+ShareMutexData<std::shared_ptr<ServerData>> g_server_data{nullptr};
+
+int main() {
+    std::cout << "client" << std::endl;
+}
