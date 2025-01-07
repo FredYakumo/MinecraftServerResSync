16e4d54c8629353f82d3d8284aa63144dd0e9f90
fredyakumo <fredyakumo@gmail.com>
2025-01-08 04:47:08 +0800

    Impl http utils > send http request

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,4 +1,6 @@
 #include "main.h"
+#include "http_utils.hpp"
+#include <boost/beast/http/verb.hpp>
 #include <iostream>
 #include <spdlog/spdlog.h>
 
@@ -14,15 +16,19 @@ using models::ManageClassPathMap;
 using models::ServerData;
 using models::ShareMutexData;
 
-ShareMutexData<std::shared_ptr<ManageClassPathMap>> g_manage_class_path_map{
-    nullptr};
+ShareMutexData<std::shared_ptr<ManageClassPathMap>> g_manage_class_path_map{nullptr};
 ShareMutexData<std::shared_ptr<ServerData>> g_server_data{nullptr};
 
 int main() {
     spdlog::set_level(spdlog::level::debug);
-    
+
     init_server_data_from_config_yaml_file("config.yaml");
     init_manage_res_hash();
-    
+
     debug("Target server host: {}", g_server_data.get_const()->host());
+
+    auto response = http_util::send_request("127.0.0.1", "/getResourceList", boost::beast::http::verb::get, "");
+    // debug("response: {}", std::string{ std::cbegin(response), std::cend(response)});
+    std::string content{response.data()};
+    info("Send http Response: {}", content);
 }
--- include/http_utils.hpp
+++ include/http_utils.hpp
@@ -1,57 +1,71 @@
 #ifndef HTTP_UTIL_H
 #define HTTP_UTIL_H
 
+#include <boost/asio/buffer.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/beast/core.hpp>
 #include <boost/beast/core/flat_buffer.hpp>
-#include <boost/beast/core/tcp_stream.hpp>
 #include <boost/beast/http.hpp>
+#include <boost/beast/http/dynamic_body.hpp>
+#include <boost/beast/http/field.hpp>
 #include <boost/beast/http/message.hpp>
 #include <boost/beast/http/string_body.hpp>
-#include <boost/beast/http/verb.hpp>
 #include <boost/beast/http/write.hpp>
+#include <boost/system/detail/error_code.hpp>
+#include <exception>
+#include <string>
 #include <string_view>
 #include <vector>
+#include <spdlog/spdlog.h>
 
 namespace http_util {
     using byte_type = char;
     namespace beast = boost::beast;
+    namespace http = beast::http;
     namespace asio = boost::asio;
 
+    std::vector<byte_type> send_request(const std::string_view host, const std::string_view path, const http::verb method, const std::string &content) {
+        try {
+            asio::io_context ioc;
 
-    std::vector<byte_type> send_request(const std::string_view host, const beast::http::verb method, const std::string &content) {
-        beast::net::io_context ioc;
-        asio::ip::tcp::resolver resolver{ioc};
-        beast::tcp_stream stream{ioc};
+            asio::ip::tcp::resolver resolver{ioc};
+            auto const results = resolver.resolve(host, "23333");
 
-        // resolve host address
-        auto const results = resolver.resolve(host, "http");
-        asio::connect(stream.socket(), results.begin(), results.end());
+            asio::ip::tcp::socket socket{ioc};
+            asio::connect(socket, results.cbegin(), results.cend());
 
-        beast::http::request<beast::http::string_body> req;
-        req.target(host);
-        req.method(method);
-        req.set(boost::beast::http::field::host, host);
-        req.set(boost::beast::http::field::user_agent, "MinecraftResSyncClient");
-        req.body() = content;
-        req.prepare_payload();
+            http::request<http::string_body> req{method, path, 11};
+            req.set(http::field::host, host);
 
+            http::write(socket, req);
 
-        boost::beast::http::write(stream, req);
+            beast::flat_buffer buffer;
 
+            std::vector<byte_type> res;
+            http::response<http::dynamic_body> response_body;
 
-        beast::flat_buffer buffer;
-        beast::http::response<boost::beast::http::string_body> res;
-        beast::http::read(stream, buffer, res);
+            http::read(socket, buffer, response_body);
 
-        beast::error_code ec;
-        stream.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
+            auto &body = response_body.body();
+            const auto &buffers = body.data();
+            for (const auto &buffer : buffers) {
+                const byte_type *data = asio::buffer_cast<const byte_type *>(buffer);
+                for (size_t i = 0; i < body.size(); ++i) {
+                    res.push_back(*(data + i));
+                }
+            }
+            boost::system::error_code ec;
+            socket.shutdown(asio::ip::tcp::socket::shutdown_both, ec);
 
-        if (ec && ec != boost::beast::errc::not_connected) {
-            throw boost::beast::system_error{ec};
+            return res;
+        } catch (const std::exception &e) {
+            spdlog::error("Send http request error: {}", e.what());
+            throw e;
         }
-
-        return std::vector<byte_type>(res.body().data(), res.body().data() + res.body().size());
     }
+
 } // namespace http_util
 
 #endif
